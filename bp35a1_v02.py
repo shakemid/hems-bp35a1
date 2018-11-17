9df123f8b86c1233484847ce35d2f3f4141a8e62
shakemid <k-cima@kendama.asia>
2018-11-17 16:05:51 +0900

    Remove loop

--- bp35a1_v02.py
+++ bp35a1_v02.py
@@ -125,57 +125,54 @@ ser.timeout = 2
 # (ECHONET-Lite_Ver.1.12_02.pdf p.4-16)
 print(ser.readline()) #無視
 
-finished = False
-while not finished :
-    # ECHONET Lite フレーム作成
-    # 　参考資料
-    # 　・ECHONET-Lite_Ver.1.12_02.pdf (以下 EL)
-    # 　・Appendix_H.pdf (以下 AppH)
-    echonetLiteFrame = b""
-    echonetLiteFrame += b"\x10\x81"      # EHD (参考:EL p.3-2)
-    echonetLiteFrame += b"\x00\x01"      # TID (参考:EL p.3-3)
-    # ここから EDATA
-    echonetLiteFrame += b"\x05\xFF\x01"  # SEOJ (参考:EL p.3-3 AppH p.3-408〜)
-    echonetLiteFrame += b"\x02\x88\x01"  # DEOJ (参考:EL p.3-3 AppH p.3-274〜)
-    echonetLiteFrame += b"\x62"          # ESV(62:プロパティ値読み出し要求) (参考:EL p.3-5)
-    echonetLiteFrame += b"\x01"          # OPC(1個)(参考:EL p.3-7)
-    echonetLiteFrame += b"\xE7"          # EPC(参考:EL p.3-7 AppH p.3-275)
-    echonetLiteFrame += b"\x00"          # PDC(参考:EL p.3-9)
+# ECHONET Lite フレーム作成
+# 　参考資料
+# 　・ECHONET-Lite_Ver.1.12_02.pdf (以下 EL)
+# 　・Appendix_H.pdf (以下 AppH)
+echonetLiteFrame = b""
+echonetLiteFrame += b"\x10\x81"      # EHD (参考:EL p.3-2)
+echonetLiteFrame += b"\x00\x01"      # TID (参考:EL p.3-3)
+# ここから EDATA
+echonetLiteFrame += b"\x05\xFF\x01"  # SEOJ (参考:EL p.3-3 AppH p.3-408〜)
+echonetLiteFrame += b"\x02\x88\x01"  # DEOJ (参考:EL p.3-3 AppH p.3-274〜)
+echonetLiteFrame += b"\x62"          # ESV(62:プロパティ値読み出し要求) (参考:EL p.3-5)
+echonetLiteFrame += b"\x01"          # OPC(1個)(参考:EL p.3-7)
+echonetLiteFrame += b"\xE7"          # EPC(参考:EL p.3-7 AppH p.3-275)
+echonetLiteFrame += b"\x00"          # PDC(参考:EL p.3-9)
 
-    # コマンド送信
-    command = "SKSENDTO 1 {0} 0E1A 1 {1:04X} ".format(ipv6Addr, len(echonetLiteFrame)).encode() + echonetLiteFrame + "\r\n".encode()
-    print(command)
-    ser.write(command)
+# コマンド送信
+command = "SKSENDTO 1 {0} 0E1A 1 {1:04X} ".format(ipv6Addr, len(echonetLiteFrame)).encode() + echonetLiteFrame + "\r\n".encode()
+print(command)
+ser.write(command)
 
-    print(ser.readline()) # エコーバック
-    print(ser.readline()) # EVENT 21 が来るはず（チェック無し）
-    print(ser.readline()) # OKが来るはず（チェック無し）
+print(ser.readline()) # エコーバック
+print(ser.readline()) # EVENT 21 が来るはず（チェック無し）
+print(ser.readline()) # OKが来るはず（チェック無し）
 
-    while True :
-        line = ser.readline()
-        print(line)
-
-        # 受信データはたまに違うデータが来たり、
-        # 取りこぼしたりして変なデータを拾うことがあるので
-        # チェックを厳しめにしてます。
-        if line.startswith(b"ERXUDP") :
-            cols = line.decode().strip().split(' ')
-            res = cols[8]   # UDP受信データ部分
-            #tid = res[4:4+4];
-            seoj = res[8:8+6]
-            #deoj = res[14,14+6]
-            ESV = res[20:20+2]
-            #OPC = res[22,22+2]
-            if seoj == "028801" and ESV == "72" :
-                # スマートメーター(028801)から来た応答(72)なら
-                EPC = res[24:24+2]
-                if EPC == "E7" :
-                    # 内容が瞬時電力計測値(E7)だったら
-                    hexPower = line[-8:]    # 最後の4バイト（16進数で8文字）が瞬時電力計測値
-                    intPower = int(hexPower, 16)
-                    print(u"瞬時電力計測値: {0} [W]".format(intPower))
-                    finished = True
-                    break
+while True :
+    line = ser.readline()
+    print(line)
+    
+    # 受信データはたまに違うデータが来たり、
+    # 取りこぼしたりして変なデータを拾うことがあるので
+    # チェックを厳しめにしてます。
+    if line.startswith(b"ERXUDP") :
+        cols = line.decode().strip().split(' ')
+        res = cols[8]   # UDP受信データ部分
+        #tid = res[4:4+4];
+        seoj = res[8:8+6]
+        #deoj = res[14,14+6]
+        ESV = res[20:20+2]
+        #OPC = res[22,22+2]
+        if seoj == "028801" and ESV == "72" :
+            # スマートメーター(028801)から来た応答(72)なら
+            EPC = res[24:24+2]
+            if EPC == "E7" :
+                # 内容が瞬時電力計測値(E7)だったら
+                hexPower = line[-8:]    # 最後の4バイト（16進数で8文字）が瞬時電力計測値
+                intPower = int(hexPower, 16)
+                print(u"瞬時電力計測値: {0} [W]".format(intPower))
+                break
 
 ser.close()
 sys.exit(0)
